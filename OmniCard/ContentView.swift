fdfd57c61a746bb0540a7e8bf00f5677f1d4acc4
Kevin Chen <73233100+kevinbevin64@users.noreply.github.com>
2025-07-27 12:53:12 -0400

    Initial Commit

---
+++ OmniCard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OmniCard
+//
+//  Created by Kevin Chen on 7/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OmniCard/OmniCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmniCardApp.swift
+//  OmniCard
+//
+//  Created by Kevin Chen on 7/27/25.
+//
+
+import SwiftUI
+
+@main
+struct OmniCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
